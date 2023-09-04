eb283bbb96fbb806e6652331457aeb600e5a6843
Naksen <khannanov.rus@gmail.com>
2023-09-04 22:21:33 +0300

    updated structure and added routers

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import JSON, Boolean, MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column
+
+from database import Base
 
 metadata = MetaData()
 
@@ -24,3 +27,11 @@ user = Table(
     Column("is_superuser", Boolean, default=False, nullable=False),
     Column("is_verified", Boolean, default=False, nullable=False),
 )
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey(role.c.id))
